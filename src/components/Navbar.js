053b22c543c7fc2e63e943f9cfe522b613f7c233
Jerome Villiseck <jerome.villiseck@outlook.com>
2024-06-14 11:30:09 +0200

    Premier evenement avec insertion d'un composant dans un autre

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,42 @@
-import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
+import React, {useState} from 'react';
+import {AppBar, Toolbar, IconButton, Typography, Button} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import SideDrawer from './SideDrawer';
 
 function Navbar() {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setIsDrawerOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setIsDrawerOpen(false);
+    };
+
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Nom de l'Application
-                </Typography>
-                <Button color="inherit">Accueil</Button>
-                <Button color="inherit">À propos</Button>
-                <Button color="inherit">Contact</Button>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                        onClick={handleDrawerOpen}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Nom de l'Application
+                    </Typography>
+                    <Button color="inherit">À propos</Button>
+                    {/* Reste du contenu de la barre de navigation */}
+                </Toolbar>
+            </AppBar>
+            <SideDrawer open={isDrawerOpen} onClose={handleDrawerClose} />
+        </>
     );
 }
 
