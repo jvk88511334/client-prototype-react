51f6964ad30309ae6b5a5d0912f9975ce6bc44c4
Jerome Villiseck <jerome.villiseck@outlook.com>
2024-06-14 11:09:29 +0200

    Première utilisation du framework mui dans un composant

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Button } from '@mui/material';
 
 function MyComponent() {
     return (
         <div>
             <h1>Mon Premier Composant</h1>
             <p>Ceci est mon premier composant React.</p>
+            <Button variant="contained" color="primary">
+                Cliquez ici
+            </Button>
         </div>
     );
 }
