9e9197be880b34e4bf7e13d60dd2a53ab4fe06bb
Jerome Villiseck <jerome.villiseck@outlook.com>
2024-06-14 11:03:01 +0200

    Premier composant, premiere vue, installation du framework ui avec npm install @mui/material @emotion/react @emotion/styled @mui/x-data-grid

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+    return (
+        <div>
+            <h1>Mon Premier Composant</h1>
+            <p>Ceci est mon premier composant React.</p>
+        </div>
+    );
+}
+
+export default MyComponent;
---
+++ src/views/HomeView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyComponent from '../components/MyComponent';
+
+function HomeView() {
+    return (
+        <div>
+            <h1>Page d'Accueil</h1>
+            <MyComponent />
+        </div>
+    );
+}
+
+export default HomeView;
